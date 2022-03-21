f5f47d5d988a52fd3344ebef0bfb0dc006469a30
khurram shafique <kshafiqu@umich.edu>
2022-03-21 05:54:51 -0400

    updated the login form

--- src/components/routing/PublicOnlyRoute.js
+++ src/components/routing/PublicOnlyRoute.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 // import AuthContext from '../../context/auth/authContext';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from "react-router-dom";
 
 const PublicOnlyRoute = ({ component: Component, ...rest }) => {
-    // const authContext = useContext(AuthContext);
-    // const { isAuthenticated, loading } = authContext;
+  // const authContext = useContext(AuthContext);
+  // const { isAuthenticated, loading } = authContext;
 
-    return (
-        <Route {...rest} render={props => localStorage.token ? (
-            <Redirect to='/' />
-        )
-            :
-            (
-                <Component {...props} />
-            )} />
-    )
-}
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.token ? <Redirect to="/" /> : <Component {...props} />
+      }
+    />
+  );
+};
 
-export default PublicOnlyRoute
+export default PublicOnlyRoute;

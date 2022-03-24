0ac05fb9f126df7195f166a9018770cdb89ea113
khurram shafique <kshafiqu@umich.edu>
2022-03-24 14:00:58 -0400

    init

--- src/components/chunks/my-profile/edit-ads/EditModalTableRow.js
+++ src/components/chunks/my-profile/edit-ads/EditModalTableRow.js
@@ -1,71 +1,99 @@
-import React, { useContext, useRef } from 'react';
-import { withRouter } from 'react-router-dom';
-import capitalize from '../../../../utils/capitalize';
-import ConfirmButton from './ConfirmButton';
-import $ from 'jquery';
-import FurnitureContext from '../../../../context/furniture/furnitureContext';
-import VehicleContext from '../../../../context/vehicle/vehicleContext';
-import MobileContext from '../../../../context/mobile/mobileContext';
-import ComputerContext from '../../../../context/computer/computerContext';
-
+import React, { useContext, useRef } from "react";
+import { withRouter } from "react-router-dom";
+import capitalize from "../../../../utils/capitalize";
+import ConfirmButton from "./ConfirmButton";
+import $ from "jquery";
+import FurnitureContext from "../../../../context/furniture/furnitureContext";
+import VehicleContext from "../../../../context/vehicle/vehicleContext";
+import MobileContext from "../../../../context/mobile/mobileContext";
+import ComputerContext from "../../../../context/computer/computerContext";
 
 const EditModalTableRow = (props) => {
-    const { _id, title, description, price, condition, index, type, removeDeletedAd, images } = props;
-    const furnitureContext = useContext(FurnitureContext);
-    const { deleteFurniture } = furnitureContext;
-    const mobileContext = useContext(MobileContext);
-    const { deleteMobile } = mobileContext;
-    const computerContext = useContext(ComputerContext);
-    const { deleteComputer } = computerContext;
-    const vehicleContext = useContext(VehicleContext);
-    const { deleteVehicle } = vehicleContext;
+  const {
+    _id,
+    title,
+    description,
+    price,
+    condition,
+    index,
+    type,
+    removeDeletedAd,
+    images,
+  } = props;
+  const furnitureContext = useContext(FurnitureContext);
+  const { deleteFurniture } = furnitureContext;
+  const mobileContext = useContext(MobileContext);
+  const { deleteMobile } = mobileContext;
+  const computerContext = useContext(ComputerContext);
+  const { deleteComputer } = computerContext;
+  const vehicleContext = useContext(VehicleContext);
+  const { deleteVehicle } = vehicleContext;
 
-    const rowRef = useRef(null);
+  const rowRef = useRef(null);
 
-    const hideRow = () => {
-        rowRef.current.hidden = true;
-    }
+  const hideRow = () => {
+    rowRef.current.hidden = true;
+  };
 
-    const deleteEvent = (itemId, type) => {
-        if (type === 'furniture')
-            deleteFurniture(itemId);
-        else if (type === 'mobile')
-            deleteMobile(itemId);
-        else if (type === 'computer')
-            deleteComputer(itemId);
-        else if (type === 'vehicle')
-            deleteVehicle(itemId);
-        removeDeletedAd(itemId, type);
-        hideRow();
-    }
+  const deleteEvent = (itemId, type) => {
+    if (type === "furniture") deleteFurniture(itemId);
+    else if (type === "mobile") deleteMobile(itemId);
+    else if (type === "computer") deleteComputer(itemId);
+    else if (type === "vehicle") deleteVehicle(itemId);
+    removeDeletedAd(itemId, type);
+    hideRow();
+  };
 
-    const editPress = () => {
-        $('#edit-products-modal').modal('hide');
-        props.history.push(`/editProduct/${type}/${_id}`);
-        console.log('product id: ' + _id, ' title: ' + title);
-    }
+  const editPress = () => {
+    $("#edit-products-modal").modal("hide");
+    props.history.push(`/editProduct/${type}/${_id}`);
+    console.log("product id: " + _id, " title: " + title);
+  };
 
-    return (
-        <tr ref={rowRef}>
-            <th scope="row">{index + 1}</th>
-            <td className="">
-                <img alt="..." src={"http://localhost:3000/images/" + images[0]} style={{ width: '30%', borderRadius: '10px' }} />
-            </td>
-            <td>{title}</td>
-            <td>{description}</td>
-            <td>{price}</td>
-            <td className="text-center">
-                <span className={"badge " + (condition && condition === "old" ? "badge-warning" : "badge-success") + " p-2"}>{condition && capitalize(condition)}</span>
-            </td>
-            <td>{type && capitalize(type)}</td>
-            <td className=""> {/* p-0 align-middle class was here */}
-                <button className="btn btn-info mr-1" onClick={editPress}>Edit</button>
-                {/* <button className="btn btn-danger" data-toggle="modal" data-target="#delete-item-modal">Delete</button> */}
-                {/* <ConfirmButton dialog={['Delete', 'Are you sure?']} action={deleteEvent(_id, type)}></ConfirmButton> */}
-                <ConfirmButton dialog={['Delete', 'Are you sure?']} action={() => { deleteEvent(_id, type) }}></ConfirmButton>
-            </td>
-        </tr>
-    )
-}
+  return (
+    <tr ref={rowRef}>
+      <th scope="row">{index + 1}</th>
+      <td className="">
+        <img
+          alt="..."
+          src={" /images/" + images[0]}
+          style={{ width: "30%", borderRadius: "10px" }}
+        />
+      </td>
+      <td>{title}</td>
+      <td>{description}</td>
+      <td>{price}</td>
+      <td className="text-center">
+        <span
+          className={
+            "badge " +
+            (condition && condition === "old"
+              ? "badge-warning"
+              : "badge-success") +
+            " p-2"
+          }
+        >
+          {condition && capitalize(condition)}
+        </span>
+      </td>
+      <td>{type && capitalize(type)}</td>
+      <td className="">
+        {" "}
+        {/* p-0 align-middle class was here */}
+        <button className="btn btn-info mr-1" onClick={editPress}>
+          Edit
+        </button>
+        {/* <button className="btn btn-danger" data-toggle="modal" data-target="#delete-item-modal">Delete</button> */}
+        {/* <ConfirmButton dialog={['Delete', 'Are you sure?']} action={deleteEvent(_id, type)}></ConfirmButton> */}
+        <ConfirmButton
+          dialog={["Delete", "Are you sure?"]}
+          action={() => {
+            deleteEvent(_id, type);
+          }}
+        ></ConfirmButton>
+      </td>
+    </tr>
+  );
+};
 
 export default withRouter(EditModalTableRow);
--- src/components/chunks/wishlist/WishDiv.js
+++ src/components/chunks/wishlist/WishDiv.js
@@ -1,31 +1,66 @@
-import React from 'react'
-import cardImage from '../../../images/card-image.jpg';
-import dateFormat from 'dateformat';
-import { Link } from 'react-router-dom';
+import React from "react";
+import cardImage from "../../../images/card-image.jpg";
+import dateFormat from "dateformat";
+import { Link } from "react-router-dom";
 
-const WishDiv = ({ id, title, price, soldFrom, datePosted, cat, deleteWish, images }) => {
-    const trashIconStyle = { position: 'absolute', top: '1%', right: '2%', cursor: 'pointer' };
-    const cardStyle = { position: 'relative', maxWidth: '18rem', cursor: 'initial' };
-    return (
-        <div className="col-xs-10 col-sm-6 col-lg-4 my-3 text-center">
-            <div className="card product-card mx-auto" style={cardStyle}>
-                <img src={"http://localhost:3000/images/"+images[0]} alt="" className="card-img-top" style={{ width: '50%', margin: 'auto' }} />
-                <div className="card-body p-1 pt-2">
-                    <hr />
-                    <h5 className="card-title font-weight-normal">{title}</h5>
-                    <span className="font-weight-bold text-secondary" style={trashIconStyle} onClick={() => { deleteWish(id) }}><i className="p-2 far fa-trash-alt text-danger wishlist-trash-icon"></i></span>
-                    <h5 className="card-subtitle my-2 text-secondary font-weight-bold">Rs. {new Intl.NumberFormat().format(price)}</h5>
-                    <Link className="mt-2 btn btn-info" to={`/product/${id}?c=${cat}`}>View Page</Link>
-                </div>
-                <div className="card-footer text-muted card-footer-address mt-3">
-                    <div className="d-flex justify-content-between">
-                        <span>{soldFrom}</span>
-                        <span>{dateFormat(datePosted, "mmm-d")}</span>
-                    </div>
-                </div>
-            </div>
+const WishDiv = ({
+  id,
+  title,
+  price,
+  soldFrom,
+  datePosted,
+  cat,
+  deleteWish,
+  images,
+}) => {
+  const trashIconStyle = {
+    position: "absolute",
+    top: "1%",
+    right: "2%",
+    cursor: "pointer",
+  };
+  const cardStyle = {
+    position: "relative",
+    maxWidth: "18rem",
+    cursor: "initial",
+  };
+  return (
+    <div className="col-xs-10 col-sm-6 col-lg-4 my-3 text-center">
+      <div className="card product-card mx-auto" style={cardStyle}>
+        <img
+          src={" /images/" + images[0]}
+          alt=""
+          className="card-img-top"
+          style={{ width: "50%", margin: "auto" }}
+        />
+        <div className="card-body p-1 pt-2">
+          <hr />
+          <h5 className="card-title font-weight-normal">{title}</h5>
+          <span
+            className="font-weight-bold text-secondary"
+            style={trashIconStyle}
+            onClick={() => {
+              deleteWish(id);
+            }}
+          >
+            <i className="p-2 far fa-trash-alt text-danger wishlist-trash-icon"></i>
+          </span>
+          <h5 className="card-subtitle my-2 text-secondary font-weight-bold">
+            Rs. {new Intl.NumberFormat().format(price)}
+          </h5>
+          <Link className="mt-2 btn btn-info" to={`/product/${id}?c=${cat}`}>
+            View Page
+          </Link>
         </div>
-    )
-}
+        <div className="card-footer text-muted card-footer-address mt-3">
+          <div className="d-flex justify-content-between">
+            <span>{soldFrom}</span>
+            <span>{dateFormat(datePosted, "mmm-d")}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default WishDiv;
